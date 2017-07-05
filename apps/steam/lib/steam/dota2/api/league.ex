5b75a66e2684fdf56f2b4292f59ac4dc96be7878
Gabriel Malkas <gabriel.malkas@gmail.com>
2017-07-05 13:56:51 -0600

    Add support for listing live Dota2 league games in Steam API client

--- apps/steam/lib/steam/dota2/api/league.ex
+++ apps/steam/lib/steam/dota2/api/league.ex
@@ -1,8 +1,16 @@
 defmodule Steam.Dota2.API.League do
-  alias Steam.Dota2.{API, League}
+  alias Steam.Dota2.{API, Game, League}
 
   def all(api_key) do
     case API.get(api_key, leagues_path()) do
+      {:ok, %{status_code: 200, body: body}} ->
+        {:ok, body |> parse_body |> parse_response}
+      error -> error
+    end
+  end
+
+  def live_games(api_key) do
+    case API.get(api_key, live_league_games_path()) do
       {:ok, %{status_code: 200, body: body}} ->
         {:ok, body |> parse_body |> parse_response}
       error -> error
@@ -19,6 +27,20 @@ defmodule Steam.Dota2.API.League do
     }
   end
 
+  def build_game(game) do
+    %Game{
+      id: game["league_game_id"],
+      number: game["game_number"],
+      league_id: game["league_id"],
+      match_id: game["match_id"],
+      dire_series_wins: game["dire_series_wins"],
+      radiant_series_wins: game["radiant_series_wins"],
+      scoreboard: game["scoreboard"],
+      radiant_team: game["radiant_team"],
+      dire_team: game["dire_team"]
+    }
+  end
+
   defp parse_body(body) do
     Poison.decode!(body)
   end
@@ -28,7 +50,16 @@ defmodule Steam.Dota2.API.League do
     |> Enum.map(&build_league/1)
   end
 
+  defp parse_response(%{"result" => %{"games" => games}}) do
+    games
+    |> Enum.map(&build_game/1)
+  end
+
   def leagues_path do
     "/GetLeagueListing/v1"
+  end
+
+  def live_league_games_path do
+    "/GetLiveLeagueGames/v1"
   end
 end
---
+++ apps/steam/lib/steam/dota2/game.ex
@@ -0,0 +1,5 @@
+defmodule Steam.Dota2.Game do
+  defstruct [:id, :number, :league_id, :match_id, :dire_series_wins,
+             :radiant_series_wins, :scoreboard, :radiant_team,
+             :dire_team]
+end
