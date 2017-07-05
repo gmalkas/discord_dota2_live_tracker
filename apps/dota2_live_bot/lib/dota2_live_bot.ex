35155ad7e9bb6accd94a52ef2279170a1082656d
Gabriel Malkas <gabriel.malkas@gmail.com>
2017-07-05 17:36:22 -0600

    Add basic Mix app for Dota2 Bot

---
+++ apps/dota2_live_bot/lib/dota2_live_bot.ex
@@ -0,0 +1,18 @@
+defmodule Dota2LiveBot do
+  @moduledoc """
+  Documentation for Dota2LiveBot.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Dota2LiveBot.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/dota2_live_bot/test/dota2_live_bot_test.exs
@@ -0,0 +1,8 @@
+defmodule Dota2LiveBotTest do
+  use ExUnit.Case
+  doctest Dota2LiveBot
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
