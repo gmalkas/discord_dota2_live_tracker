caedc1a4a9638fd194e0357cb5e59789f90c7a13
Gabriel Malkas <gabriel.malkas@gmail.com>
2017-07-08 17:21:36 -0600

    Add a more readable name for leagues

--- apps/steam/lib/steam/dota2/league.ex
+++ apps/steam/lib/steam/dota2/league.ex
@@ -1,3 +1,11 @@
 defmodule Steam.Dota2.League do
   defstruct [:id, :description, :name, :tournament_url, :itemdef]
+
+  alias __MODULE__
+
+  def human_readable_name(%League{name: name}) do
+    name
+    |> String.replace_leading("#DOTA_Item", "")
+    |> String.replace("_", " ")
+  end
 end
